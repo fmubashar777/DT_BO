b4c95824df246a07d1f5e7c10447363d2072c7f3
fmubashar777 <fmubashar777@gmail.com>
2022-03-07 19:26:45 +0500

    few lines added

---
+++ practice.r
@@ -0,0 +1,29 @@
+a =5
+class(a)
+
+a =5L
+class(a)
+
+b =5.67
+class(b)
+
+x <- 1
+print(x)
+
+nafi <- "multilingual platform"
+print(nafi)  
+
+x <- 1:30
+x
+/// creating vectors///
+x <- c("a", "b", "c")
+x
+
+x <- c("d", length = 10)
+x
+
+y <- c(1.2, 'a')
+y
+
+as.logical(y)
+as.numeric(x)
